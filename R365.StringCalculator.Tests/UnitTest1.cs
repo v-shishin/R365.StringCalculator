58670b9f899d013af497706cc70c4d62fe05fd7b
Shishin <Vladimir_Shishin@CORVEL.com>
2019-11-23 15:40:06 -0800

    created empty Console and Unit Test projects

---
+++ R365.StringCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace R365.StringCalculator.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ R365.StringCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R365.StringCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
